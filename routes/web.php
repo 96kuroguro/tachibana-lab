72c5fd8624e12f59b01b52fe5c12860ac10d012a
kuroguro <96kuroguro@gmail.com>
2018-04-09 12:05:27 +0900

    twitter api 取得テスト

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/twitter', 'TwitterController@index');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
