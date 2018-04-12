efa47ba28e95ef771b40df67086d667d3d147127
kuroguro <96kuroguro@gmail.com>
2018-04-12 10:30:18 +0900

    OPオマージュ作成

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,22 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-
-                <div class="card-body">
                     <div class="day-wrap">
                         <p class="day">プレゼント・デイ</p>
                     </div>
                     <div class="time-wrap">
                         <p class="time">プレゼント・タイム</p>
                     </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
 </div>
 @endsection
 
