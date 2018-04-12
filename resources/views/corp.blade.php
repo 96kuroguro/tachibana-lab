c35d80b6377b4cb51ea88ee287ef7e6e3656264a
96kuroguro <96kuroguro@gmail.com>
2018-04-12 14:46:38 +0900

    橘総研情報入力

--- resources/views/corp.blade.php
+++ resources/views/corp.blade.php
@@ -19,43 +19,43 @@
     <div class="container">
       <header class="blog-header py-3">
         <div class="row flex-nowrap justify-content-between align-items-center">
-          <div class="col-4 pt-1">
+          <!-- <div class="col-4 pt-1">
             <a class="text-muted" href="#">Subscribe</a>
-          </div>
-          <div class="col-4 text-center">
+          </div> -->
+          <div class="col-4 offset-4 text-center">
             <a class="blog-header-logo text-dark" href="#">橘総研</a>
           </div>
-          <div class="col-4 d-flex justify-content-end align-items-center">
+          <!-- <div class="col-4 d-flex justify-content-end align-items-center">
             <a class="text-muted" href="#">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
             </a>
             <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
-          </div>
+          </div> -->
         </div>
       </header>
 
       <div class="nav-scroller py-1 mb-2">
         <nav class="nav d-flex justify-content-between">
-          <a class="p-2 text-muted" href="#">World</a>
-          <a class="p-2 text-muted" href="#">U.S.</a>
-          <a class="p-2 text-muted" href="#">Technology</a>
-          <a class="p-2 text-muted" href="#">Design</a>
-          <a class="p-2 text-muted" href="#">Culture</a>
-          <a class="p-2 text-muted" href="#">Business</a>
-          <a class="p-2 text-muted" href="#">Politics</a>
-          <a class="p-2 text-muted" href="#">Opinion</a>
-          <a class="p-2 text-muted" href="#">Science</a>
-          <a class="p-2 text-muted" href="#">Health</a>
-          <a class="p-2 text-muted" href="#">Style</a>
-          <a class="p-2 text-muted" href="#">Travel</a>
+          <a class="p-2" href="#navi">NAVI</a>
+          <a class="p-2" href="#handnavi">Hand NAVI</a>
+          <a class="p-2" href="#os">Copland OS</a>
+          <a class="p-2 text-muted" href="">Processors</a>
+          <a class="p-2 text-muted" href="">Boards</a>
+          <a class="p-2 text-muted" href="">Memory</a>
+          <a class="p-2" href="/op">WIRED</a>
+          <a class="p-2 text-muted" href="">Support</a>
+          <a class="p-2" href="#aboutus">About Us</a>
+          <a class="p-2 text-muted" href="">Contact Us</a>
+          <a class="p-2 text-muted" href="">Shopping</a>
         </nav>
       </div>
 
       <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
         <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-          <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
-          <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+          <h1 class="display-4 font-italic">Copland OS Enterprise</h1>
+          <p class="lead my-3">version 4.2 New Release</p>
+          <p class="lead my-3">ワイヤードに新しい時代が訪れる</p>
+          <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">詳細はこちら</a></p>
         </div>
       </div>
 
@@ -63,13 +63,15 @@
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-primary">World</strong>
+              <strong class="d-inline-block mb-2 text-primary">NAVI</strong>
               <h3 class="mb-0">
-                <a class="text-dark" href="#">Featured post</a>
+                <a class="text-dark" href="#">Copland OS</a>
               </h3>
-              <div class="mb-1 text-muted">Nov 12</div>
-              <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-              <a href="#">Continue reading</a>
+              <div class="mb-1 text-muted">v4.2 エンタープライズ版</div>
+              <p class="card-text mb-auto">7月7日に販売決定したCopland OSの最新版<br>
+              ワイヤードへのリアルタイムレンダリングも最大3kGb/s<sup>※</sup>で全身投影もストレスフリーで実現可能</p>
+              <p class="text-muted">(※ベストエフォート)</p>
+              <a href="">製品詳細はこちら</a>
             </div>
             <img class="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap">
           </div>
@@ -77,7 +79,7 @@
         <div class="col-md-6">
           <div class="card flex-md-row mb-4 box-shadow h-md-250">
             <div class="card-body d-flex flex-column align-items-start">
-              <strong class="d-inline-block mb-2 text-success">Design</strong>
+              <strong class="d-inline-block mb-2 text-success">Hand NAVI</strong>
               <h3 class="mb-0">
                 <a class="text-dark" href="#">Post title</a>
               </h3>
