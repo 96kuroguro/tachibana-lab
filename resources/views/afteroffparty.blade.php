8beca9e19fccb6bb574fd0e1874e1a1ee0060e17
kuroguro <96kuroguro@gmail.com>
2018-06-20 19:05:01 +0900

    アンケート公開

--- resources/views/afteroffparty.blade.php
+++ resources/views/afteroffparty.blade.php
@@ -76,44 +76,38 @@
               ドリンクと軽食、お菓子をご用意いたします。<br>
               ご自身で飲みたいお酒や、食べたい食事などの<b class="text-danger">持ち込みも可能</b>です。<br>
               <br>
-              参加者に配れる差し入れも大歓迎です。（ナマモノはご遠慮ください）<br>
-              ただし、お酒を持ち込む際は、ご自身はもちろん、周りの方の飲み過ぎにもご配慮ください。<br>
+              参加者に配れる差し入れも大歓迎です。<br>
+              <br>
+              ただし、お酒を持ち込む際は、ご自身はもちろん、周りの方の飲み過ぎにもご配慮ください。生モノはご遠慮ください。<br>
               余った場合の持ち帰りにもご協力お願いします。<br>
               <br>
-              {{--
               会場の個室を更衣室として使うことも可能ですので、<b class="text-danger">コスプレ希望の方は備考欄にご記入ください。</b><br>
               <br>
               当日参加枠も可能であれば用意しますが、混み状況によっては入場できない場合もあります。<br>
-              当日参加希望の方は必ず申し込みをして、返信が来てからご来場ください。<br>
-              --}}
+              当日参加希望の方も必ず申し込みをして、返信が来てからご来場ください。<br>
             </p>
             <br>
-            {{--
-            <div class="progress">
-              <div class="progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
+            <hr>
             <br>
 
             <div class="lain-font">
               <span class="font-weight-bold text-danger">present day : </span><br>
-              7/8 sun<br>
+              2018 7/8 sun<br>
               <br>
               <span class="font-weight-bold text-danger">present time : </span><br>
               Part 1　open 10:30　/ close 13:00<br>
               Part 2　open 13:30　/ close 16:00<br>
               <br>
               <span class="font-weight-bold text-danger">place : </span><br>
-              <a href="https://na-na-q.com/contact/">SAKEビストロ NA-NA</a><br>
+              恵比寿 <a href="https://na-na-q.com/contact/">SAKEビストロ NA-NA</a><br>
               ※スペースだけをお借りしているので、食事やお酒などお店からの提供はありません。<br>
               <br>
               <span class="font-weight-bold text-danger">price : </span><br>
               2,000円<br>
-              お酒を飲まれる方は＋500円頂戴します。<br>
+              ※お酒を飲まれる方は＋500円頂戴します。<br>
             </div>
             <br>
-            <div class="progress">
-              <div class="progress-bar progress-bar-striped bg-secondary" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
+            <hr>
             <br>
 
             <p>
@@ -127,20 +121,18 @@
               <a href="https://docs.google.com/forms/d/e/1FAIpQLSejBWBbXYtsT-7IHsY9yPE-a-hCiEC2Ujs46HMSxSB2xf2qDQ/viewform?usp=sf_link" class="btn btn-danger btn-block btn-lg">事前申込者用アンケートはこちら</a><br>
               <br>
               <span class="text-danger">※ただいま、事前申し込みされた方へアンケートを取っております。<br>
-              結果により追加でご参加いただける人数が変動しますのでしばらくお待ち下さい。</span><br>
-              <a href="" class="btn btn-info btn-block btn-lg disabled">参加申込みはこちら</a><br>
-              --}}
+              結果により追加でご参加いただける人数が前後しますのでしばらくお待ち下さい。</span><br>
 
               <p>
-              ただいま申込みの準備しています。<br>
-              <br>
               あと・・少し、待って
               </p>
 
-            <br>
-            <div class="progress">
-              <div class="progress-bar progress-bar-striped bg-secondary  progress-bar-animated" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
+              <div class="progress">
+                <div class="progress-bar progress-bar-striped bg-secondary  progress-bar-animated" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+              <br>
+
+              <a href="" class="btn btn-info btn-block btn-lg disabled">参加申込みはこちら</a><br>
 
           </div><!-- /.blog-post -->
 
