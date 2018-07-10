1ce4219b8dca43fe8505445a4081a61c886a3c2a
kuroguro <96kuroguro@gmail.com>
2018-07-10 13:26:41 +0900

    メニュー変更

--- resources/views/include/menu.blade.php
+++ resources/views/include/menu.blade.php
@@ -1,12 +1,12 @@
 <div class="nav-scroller py-1 mb-2">
     <nav class="nav d-flex justify-content-between">
         <a class="p-2" href="{{ url('afteroffparty') }}">アフターオフ会</a>
+        <a class="p-2" href="{{ url('ss') }}">ファンアートSS</a>
         <a class="p-2" href="{{ url('/') }}#navi">NAVI</a>
         <a class="p-2" href="{{ url('/') }}#handnavi">Handy NAVI</a>
         <a class="p-2" href="{{ url('/') }}#os">Copland OS</a>
         <a class="p-2 text-muted" href="" style="pointer-events: none;">Processors</a>
         <a class="p-2 text-muted" href="" style="pointer-events: none;">Boards</a>
-        <a class="p-2 text-muted" href="" style="pointer-events: none;">Memory</a>
         <a class="p-2" href="{{ url('op') }}">WIRED</a>
         <a class="p-2 text-muted" href="" style="pointer-events: none;">Support</a>
         <a class="p-2" href="{{ url('/') }}#aboutus">About Us</a>
