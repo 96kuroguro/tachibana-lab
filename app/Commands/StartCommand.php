fee051db2d4f3fe53f851e39fc98c70a8c760709
96kuroguro <96kuroguro@gmail.com>
2018-10-08 19:49:58 +0900

    NAVI起動画像作成

--- app/Commands/StartCommand.php
+++ app/Commands/StartCommand.php
@@ -21,7 +21,7 @@ class StartCommand extends Command
     /**
      * @var string Command Description
      */
-    protected $description = 'Start Command to get you started';
+    protected $description = '新規スタート';
 
     /**
      * {@inheritdoc}
@@ -61,8 +61,8 @@ class StartCommand extends Command
             Keyboard::button(['text' => 'NAVIを起動する'])
         );
 
-        $this->replyWithMessage(['text' => "あなたはNAVIを起動してあるメッセージを受け取ります。\n会話を進めた結果、あなたは最後にどのような存在になるでしょう？", 'reply_markup' => $keyboard]);
-
+        // $this->replyWithMessage(['text' => "あなたはNAVIを起動してあるメッセージを受け取ります。\n会話を進めた結果、あなたは最後にどのような存在になるでしょう？", 'reply_markup' => $keyboard]);
+        $this->replyWithPhoto(['photo'=>asset('img/chisayomoda_bot/navi_off.jpg'), 'reply_markup' => $keyboard]);
 
     }
 
