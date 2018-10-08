0b0ba5c5150754a77f8830283a33d73721b09d04
96kuroguro <96kuroguro@gmail.com>
2018-10-08 20:18:55 +0900

    test

--- app/Commands/StartCommand.php
+++ app/Commands/StartCommand.php
@@ -63,7 +63,7 @@ class StartCommand extends Command
 
         // $this->replyWithMessage(['text' => "あなたはNAVIを起動してあるメッセージを受け取ります。\n会話を進めた結果、あなたは最後にどのような存在になるでしょう？", 'reply_markup' => $keyboard]);
         $this->replyWithPhoto([
-            'photo' => asset('img/chisayomoda_bot/navi_off.jpg'), 
+            'photo' => InputFile::create(asset('img/chisayomoda_bot/navi_off.jpg'), 'NAVI'), 
             'caption' => '',
             'reply_markup' => $keyboard
             ]);
--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/test', function () {
     $res = Telegram::getMe();
-    dd(asset('img/chisayomoda_bot/navi_off.jpg'));
+    dd(InputFile::create(asset('img/chisayomoda_bot/navi_off.jpg'), 'NAVI'));
 });
 
 
