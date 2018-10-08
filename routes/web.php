74472cad0b48f40f9b3cea1dd8bc1aa4f4f8b544
96kuroguro <96kuroguro@gmail.com>
2018-10-08 20:21:00 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/test', function () {
     $res = Telegram::getMe();
-    dd(InputFile::create(asset('img/chisayomoda_bot/navi_off.jpg'), 'NAVI'));
+    dd(File::create(asset('img/chisayomoda_bot/navi_off.jpg'), 'NAVI'));
 });
 
 
