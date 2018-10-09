2cc8fb5bcb3ac3e760d3904d772e66a2000baa76
96kuroguro <96kuroguro@gmail.com>
2018-10-09 14:31:40 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -74,7 +74,7 @@ Route::get('/set', function () {
 Route::post('/'.config('telegram.bots.mybot.token').'/webhook', function () {
     $update = Telegram::commandsHandler(true);
 
-    if ($update->isType('callback_data')) {
+    if ($update->isType('callback_query')) {
 
         $message = $update->getMessage();    
         $chatId = $message->getChat()->getId();
