b920d871ed2fac09b55f7e27f3e4f157a0d4a4cc
kuroguro <96kuroguro@gmail.com>
2018-10-10 14:36:00 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::post('/'.config('telegram.bots.mybot.token').'/webhook', function () {
     $chatId = $message->getChat()->getId();
 
     // $rs_message = var_export($update->getMessage(), true);
-    $rs_array = var_export($update['items']['callback_query']['data'], true);
+    $rs_array = var_export($update['items'], true);
     // Telegram::sendMessage([
     //     'chat_id'  =>  $chatId, 
     //     'text'  =>  $rs_message
